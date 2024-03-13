fbdbcd2a184364e694970a41abcb400e2dff3e8f
SANJAYSHANKAR3 <sanjayshankarjayaraman@gmail.com>
2024-03-13 16:02:55 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ProductListComponent } from '../component/product-list/product-list.component';
+import { HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ProductListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/component/product-list/product-list.component.ts
+++ src/component/product-list/product-list.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common'
+import { ProductService } from '../../app/service/product.service';
+import { Product } from '../../model/product';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+  products: Product[] = [];
+  constructor(
+    private productService: ProductService) {
+    this.loadAllProducts();
+  }
 
+  loadAllProducts() {
+    this.productService.getAllProducts()
+      .subscribe(
+        {
+          next: (data) => {
+            console.log(data);
+            this.products = data;
+          },
+          error: (error) => {
+            console.log(error);
+          },
+          complete: () => {
+            console.log("Completed data loading");
+          }
+        }
+      )
+  }
 }
