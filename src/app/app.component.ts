7df3a72e851c609ab362c039e06b91cd6799de6b
SANJAYSHANKAR3 <sanjayshankarjayaraman@gmail.com>
2024-03-14 18:17:29 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from '../component/product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from '../component/header/header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ProductListComponent],
+  imports: [RouterOutlet,ProductListComponent,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
